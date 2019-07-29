b937c756b9f72c0a8ea7fe12371ec9a10e649b75
Farkhod <sadykovfarkhod@gmail.com>
2019-07-28 20:03:42 -0500

    Signup added to check go to /signup

--- artemis.py
+++ artemis.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
