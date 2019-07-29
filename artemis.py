2e739249504a4478ca01caf9423e1863e681afff
Farkhod <sadykovfarkhod@gmail.com>
2019-07-28 19:55:30 -0500

    Created version one application

---
+++ artemis.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World'
+
+
+if __name__ == '__main__':
+    app.run()
