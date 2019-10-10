93db475623322c805b45ca414c919bcd074bf928
Caglar Cavdar <chaglare@gmail.com>
2019-10-09 19:54:59 -0500

    edited

--- deployment/terraform/artemis-helmrelease.tf
+++ deployment/terraform/artemis-helmrelease.tf
@@ -1,7 +1,8 @@
-# data "helm_repository" "stable" {
-#     name = "stable"
-#     url  = "https://kubernetes-charts.storage.googleapis.com"
-# }
+data "helm_repository" "stable" {
+    name = "stable"
+    url  = "https://github.com/fuchicorp"
+}
+
 resource "helm_release" "artemis" {
   name       = "artemis-release"
   repository = "${file("./artemis-deployment/Chart.yaml")}"
